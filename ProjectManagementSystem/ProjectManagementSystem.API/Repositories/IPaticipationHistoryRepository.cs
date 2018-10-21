11b8899e712989b6f79e0f113e905f87b2d28165
dima-volkovitch <dima.volkovitch@yandex.by>
2018-10-21 18:29:32 +0300

    Create paticipation history repository

---
+++ ProjectManagementSystem/ProjectManagementSystem.API/Repositories/IPaticipationHistoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using ProjectManagementSystem.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManagementSystem.API.Repositories
+{
+    public interface IPaticipationHistoryRepository : IRepository<PaticipationHistory>
+    {
+    }
+}
